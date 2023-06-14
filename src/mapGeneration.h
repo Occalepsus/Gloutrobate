efbbf2ed84bb157a0dc168eb39558360ec24bf3f
tihine <victorine.casse@telecom-sudparis.eu>
2023-06-14 22:29:42 +0200

    Add the sprites

---
+++ src/mapGeneration.h
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include <random>
+#include "Engine/Engine.h"
+
+class Map {
+private : 
+
+public : 
+	void generation(gloutrobate::Engine &const game);
+	gloutrobate::GameObject creationPlatform(gloutrobate::Engine &const game);
+};
